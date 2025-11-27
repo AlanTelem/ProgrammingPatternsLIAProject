f22c50116710b3770e2a24154df4b5980a309842
AlanTelem <deleteriouseuph@gmail.com>
2025-11-27 11:45:32 -0500

    Add files via upload

---
+++ src/patternsprojectforms/PatternsProjectForms.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package patternsprojectforms;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author lukec
+ */
+public class PatternsProjectForms extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("CollectionMenu.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Collections Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    
+}
